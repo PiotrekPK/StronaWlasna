d7b19c57eb36d6dae57195136fd8572c39a12f73
Piotr Kwiatek <piotrekwiatek2004@gmail.com>
2022-12-07 22:00:59 +0100

    mod10

--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(".section");
+const sections = document.querySelectorAll(".section");
+const div = document.querySelector(".bl");
 
-  let currentSectionIndex = 0;
+sections[0].scrollIntoView({
+  behavior: "smooth",
+  block: "start",
+});
+let currentSectionIndex = 0;
+let flag = false;
 
-  document.addEventListener("mousewheel", (e) => {
-    let way = e.wheelDelta < 0 ? 1 : -1;
-    if (way === 1) {
-      const isLastSection = currentSectionIndex === sections.length - 1;
-      if (isLastSection) return;
-    } else {
-      const isLastSection = currentSectionIndex === 0;
-      if (isLastSection) return;
-    }
-    currentSectionIndex = currentSectionIndex + way;
-    console.log(currentSectionIndex);
-    console.log(sections[currentSectionIndex]);
-    sections[currentSectionIndex].scrollIntoView(true);
+document.addEventListener("mousewheel", function (e) {
+  let way = e.wheelDelta < 0 ? 1 : -1;
+  if (flag === true) {
+    return;
+  }
+  flag = true;
+  setTimeout(() => {
+    flag = false;
+  }, 500);
+  if (way === 1) {
+    const isLastSection = currentSectionIndex === sections.length - 1;
+    if (isLastSection) return;
+  } else {
+    const isLastSection = currentSectionIndex === 0;
+    if (isLastSection) return;
+  }
+  currentSectionIndex = currentSectionIndex + way;
+  sections[currentSectionIndex].scrollIntoView({
+    behavior: "smooth",
+    block: "start",
   });
+});
+
+window.addEventListener("mousemove", (e) => {
+  console.log(e.clientX);
+  console.log(e.clientY);
+  div.style.transform = `translate(${(e.clientX / 100) * -1}px, ${
+    (e.clientY / 100) * -1
+  }px)`;
 });
